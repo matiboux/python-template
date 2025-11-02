5b7547d857c5623f57a5523ddcdc3243389c3803
Matiboux <matiboux@gmail.com>
2025-11-02 03:21:43 +0100

    Start simple pure hTTP server

--- app/app/main.py
+++ app/app/main.py
@@ -1,5 +1,17 @@
 def main():
-    print("Hello from app!")
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
+    class HelloHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, world!')
+
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, HelloHandler)
+    print("Serving on http://localhost:8000 ...")
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
