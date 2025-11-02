a0649e177d636d0db34bdfd97d856b5a0a4a77db
Matiboux <matiboux@gmail.com>
2025-11-02 04:03:40 +0100

    Fix lint errors

--- app/app/main.py
+++ app/app/main.py
@@ -1,12 +1,15 @@
 """Simple HTTP server module that responds with 'Hello, world!' to all GET requests."""
 
-import os
 import logging
+import os
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 
 class HelloHandler(BaseHTTPRequestHandler):
+	"""Request handler that responds with 'Hello, world!' to all GET requests."""
+
 	def do_GET(self) -> None:
+		"""Handle GET requests by responding with 'Hello, world!'."""
 		self.send_response(200)
 		self.send_header('Content-type', 'text/plain')
 		self.end_headers()
@@ -16,10 +19,13 @@ class HelloHandler(BaseHTTPRequestHandler):
 def main() -> None:
 	"""Start a threaded HTTP server that responds with 'Hello, world!' to all GET requests."""
 	logging.basicConfig(level=logging.INFO)
+	logger = logging.getLogger(__name__)
 	http_port = int(os.getenv('PORT', '8080'))
 	httpd = ThreadingHTTPServer(('0.0.0.0', http_port), HelloHandler)
-	logging.info(f"Serving HTTP on 0.0.0.0 port {http_port} (http://0.0.0.0:{http_port}/) ...")
-	logging.info(f"Open http://localhost:{http_port} locally in your browser.")
+	logger.info(
+		"Serving HTTP on 0.0.0.0 port %d (http://0.0.0.0:%d/) ...", http_port, http_port
+	)
+	logger.info("Open http://localhost:%d locally in your browser.", http_port)
 	httpd.serve_forever()
 
 
