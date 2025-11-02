5d4f5313ca5d1552f3ef551ced3722a8a55eb078
Matiboux <matiboux@gmail.com>
2025-11-02 04:31:09 +0100

    Explicitly shutdown the test http server

--- app/test/test_server.py
+++ app/test/test_server.py
@@ -1,20 +1,23 @@
+
 import threading
 import time
 import httpx
 import os
 import pytest
-from app.main import main
+from app.main import HelloHandler
+from http.server import ThreadingHTTPServer
 
 
 @pytest.fixture(scope="module", autouse=True)
 def start_server():
-    # Set the port for the test
-    os.environ["PORT"] = os.environ.get("TEST_PORT", "8081")
-    server_thread = threading.Thread(target=main, daemon=True)
+    port = int(os.environ.get("TEST_PORT", "8081"))
+    httpd = ThreadingHTTPServer(("127.0.0.1", port), HelloHandler)
+    server_thread = threading.Thread(target=httpd.serve_forever)
     server_thread.start()
     time.sleep(0.5)  # Give server time to start
     yield
-    # No explicit shutdown since daemon=True
+    httpd.shutdown()
+    server_thread.join()
 
 def test_hello_world():
     port = int(os.environ.get('TEST_PORT', '8081'))
