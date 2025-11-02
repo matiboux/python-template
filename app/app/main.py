f48df65472f9a9fe407df5c51c02e8965c3c68bf
Matiboux <matiboux@gmail.com>
2025-11-02 03:59:37 +0100

    Fix lint errors

--- app/app/main.py
+++ app/app/main.py
@@ -1,20 +1,27 @@
+"""Simple HTTP server module that responds with 'Hello, world!' to all GET requests."""
+
+import os
+import logging
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+
+
+class HelloHandler(BaseHTTPRequestHandler):
+	def do_GET(self) -> None:
+		self.send_response(200)
+		self.send_header('Content-type', 'text/plain')
+		self.end_headers()
+		self.wfile.write(b'Hello, world!')
+
+
 def main() -> None:
-	import os
-	from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-
-	class HelloHandler(BaseHTTPRequestHandler):
-		def do_GET(self) -> None:
-			self.send_response(200)
-			self.send_header("Content-type", "text/plain")
-			self.end_headers()
-			self.wfile.write(b"Hello, world!")
-
-	http_port = int(os.getenv("PORT", "8080"))
-	httpd = ThreadingHTTPServer(("0.0.0.0", http_port), HelloHandler)
-	print(f"Serving HTTP on 0.0.0.0 port {http_port} (http://0.0.0.0:{http_port}/) ...")
-	print(f"Open http://localhost:{http_port} locally in your browser.")
+	"""Start a threaded HTTP server that responds with 'Hello, world!' to all GET requests."""
+	logging.basicConfig(level=logging.INFO)
+	http_port = int(os.getenv('PORT', '8080'))
+	httpd = ThreadingHTTPServer(('0.0.0.0', http_port), HelloHandler)
+	logging.info(f"Serving HTTP on 0.0.0.0 port {http_port} (http://0.0.0.0:{http_port}/) ...")
+	logging.info(f"Open http://localhost:{http_port} locally in your browser.")
 	httpd.serve_forever()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
